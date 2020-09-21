57a41cf1161cc32a3517dc0bcbb3e16d789379bd
MaxSpanier <maxspanier17@gmail.com>
2020-09-21 12:16:25 +0200

    Added some basics

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, url_for, redirect, request
 from app import app, db
 from datetime import datetime
+from bson.objectid import ObjectId
 
 
 # https://flask-recipe-book.s3.eu-central-1.amazonaws.com/{{ recipe.image }}.jpg
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    entry = db.recipes.find_one({"title": "Brownies"})
-    return render_template("home.html", entry=entry)
+    recipe = db.recipes.find_one({"title": "Brownies"})
+    return render_template("home.html", recipe=recipe)
 
 @app.route("/add_recipe", methods=['GET', 'POST'])
 def add_recipe():
@@ -18,7 +19,7 @@ def add_recipe():
 def insert_recipe():
     recipe_title = request.form['title']
     recipe_description = request.form['description']
-    recipe_method = request.form['method']
+    recipe_method = request.form['method'].split(';')
     recipe_ingredients = request.form.getlist('ingredient')
     recipe_meal = request.form.get('meal')
     recipe_serves = request.form['serves']
@@ -36,11 +37,21 @@ def insert_recipe():
     "ingredients": recipe_ingredients,
     "meal": recipe_meal,
     "serves": recipe_serves,
-    "cooking-time": recipe_cooking_time,
-    "prep-time": recipe_prep_time,
+    "cooking_time": recipe_cooking_time,
+    "prep_time": recipe_prep_time,
     "image": recipe_image,
     "last_modified": current_time
     }
 
     db.recipes.insert_one(recipe_form)
     return redirect(url_for('home'))
+
+@app.route("/all_recipes")
+def all_recipes():
+    recipes = db.recipes.find()
+    return render_template("all_recipes.html", recipes=recipes)
+
+@app.route("/recipe/<recipe_id>")
+def recipe(recipe_id):
+    recipe = db.recipes.find({"_id": ObjectId(recipe_id)})
+    return render_template("recipe.html", recipe=recipe)
